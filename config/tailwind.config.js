25a405d26726b34e3156083867c0166b042c019d
Jesse Spevack <jspevack@gmail.com>
2025-02-22 23:47:46 -0700

    Feedback on landing page

--- config/tailwind.config.js
+++ config/tailwind.config.js
@@ -7,6 +7,10 @@ module.exports = {
   ],
   theme: {
     extend: {
+      fontFamily: {
+        sans: ['DM Sans', 'system-ui', 'sans-serif'],
+        display: ['Fraunces', 'serif']
+      },
       fontSize: {
         'mega': ['20rem', { lineHeight: '1' }],
         'super': ['25rem', { lineHeight: '1' }]
