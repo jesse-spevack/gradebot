56acd1f4a214004678c1eb1726d37f6eaa7746d0
Jesse Spevack <jspevack@gmail.com>
2025-03-14 22:20:58 -0600

    Clean up

--- config/initializers/solid_queue.rb
+++
@@ -1,5 +0,0 @@
-# Simple initializer - the worker and dispatcher configuration will be loaded
-# from queue.yml by default in Rails 8
-
-# Log when starting - useful for debugging
-Rails.logger.info "SolidQueue initializer loaded"
