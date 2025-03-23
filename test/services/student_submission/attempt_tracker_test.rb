f4f3e0f52b6e8a33348198a073651728b21b1da7
Jesse Spevack <jspevack@gmail.com>
2025-03-23 12:50:37 -0600

    Test fix

--- test/services/student_submission/attempt_tracker_test.rb
+++ test/services/student_submission/attempt_tracker_test.rb
@@ -17,20 +17,22 @@ class StudentSubmission::AttemptTrackerTest < ActiveSupport::TestCase
   end
 
   test "it tracks student submission attempts but does not change first_attempted_at timestamp" do
-    now = Time.current
-    student_submission = StudentSubmission.create(
-      grading_task: grading_tasks(:one),
-      original_doc_id: "12345",
-      status: :pending,
-      first_attempted_at: now
-    )
+    freeze_time do
+      now = Time.current
+      student_submission = StudentSubmission.create(
+        grading_task: grading_tasks(:one),
+        original_doc_id: "12345",
+        status: :pending,
+        first_attempted_at: now
+      )
 
-    assert_equal(now, student_submission.first_attempted_at)
-    assert_equal(0, student_submission.attempt_count)
+      assert_equal(now, student_submission.first_attempted_at)
+      assert_equal(0, student_submission.attempt_count)
 
-    student_submission = StudentSubmission::AttemptTracker.track(student_submission)
+      student_submission = StudentSubmission::AttemptTracker.track(student_submission)
 
-    assert_equal(now, student_submission.first_attempted_at)
-    assert_equal(1, student_submission.attempt_count)
+      assert_equal(now, student_submission.first_attempted_at)
+      assert_equal(1, student_submission.attempt_count)
+    end
   end
 end
