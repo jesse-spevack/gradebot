2c5b24b799ca8f635ba13772fc7eaa5df7ff6205
Jesse Spevack <jspevack@gmail.com>
2025-03-30 09:26:14 -0600

    remove controller test

--- test/controllers/features_controller_test.rb
+++
@@ -1,8 +0,0 @@
-require "test_helper"
-
-class FeaturesControllerTest < ActionDispatch::IntegrationTest
-  test "should get index" do
-    get features_index_url
-    assert_response :success
-  end
-end
